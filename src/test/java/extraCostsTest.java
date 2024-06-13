70101d979440836523c89e6a16f7d64b40774806
grgbishan <gurungbishan22@gmail.com>
2024-06-13 16:47:52 +0545

    updates in extracost page and finilizing extra cost

--- src/test/java/extraCostsTest.java
+++ src/test/java/extraCostsTest.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.ExtraCostsPage;
 import pages.LoginPage;
@@ -35,47 +37,102 @@ public class extraCostsTest {
 
     @Test
     public void verifyThatTheAddNewBtnOfExtraCostsOpensAddNewPage() {
-        addBtnIsVisible(addNewBtn);
-        driver.findElement(addNewBtn).click();
+        inputBoxIsVisible(addNewBtn);
         String addPageText = driver.findElement(addNewPageText).getText();
         Assertions.assertEquals("Add Extra Costs", addPageText);
     }
 
     @Test
-    public void verifyThatTheDateIsNotNull() throws InterruptedException {
-        addBtnIsVisible(addNewBtn);
-        driver.findElement(addNewBtn).click();
-        driver.findElement(pickDate).click();
-        selectDate();
-        Assertions.assertNotNull(driver.findElement(pickDate).getAttribute("value"), "Date should not be null");
+    public void verifyThatTheDateIsNotNull() {
+        inputBoxIsVisible(addNewBtn);
+        Assertions.assertNotNull(pickDate);
     }
 
     @Test
     public void verifyThatTheBookingIdIsNotNull() {
-        addBtnIsVisible(addNewBtn);
-        driver.findElement(addNewBtn).click();
-        Assertions.assertNotNull(driver.findElement(bookingId).getAttribute("value"), "Booking ID should not be null");
+        inputBoxIsVisible(addNewBtn);
+        Assertions.assertNotNull(bookingId);
     }
 
     @Test
-    public void verifyThatTheRoomTitleIsNotNull() throws InterruptedException {
-        addBtnIsVisible(addNewBtn);
-        driver.findElement(addNewBtn).click();
-        Assertions.assertNotNull(driver.findElement(roomTitleInput).getAttribute("value"), "Room Title should not be null");
+    public void verifyThatTheRoomTitleIsNotNull() {
+        inputBoxIsVisible(addNewBtn);
+        Assertions.assertNotNull(roomTitleInput);
     }
 
     @Test
-    public void verifyThatTheExtraCostsIsAdded() throws InterruptedException {
-        addBtnIsVisible(addNewBtn);
-        addExtraCost();
+    public void verifyThatTheExtraCostsIsNotSearchableWithInValidData() throws InterruptedException {
+        inputBoxIsVisible(addNewBtn);
+        searchRoomForExtraCost("6", roomTitleKeys);
+        String bookingDetails = driver.findElement(error_msg).getText();
+        Assertions.assertEquals("Room not found", bookingDetails);
+    }
+
+    @Test
+    public void verifyThatTheExtraCostsIsSearchableWithValidData() throws InterruptedException {
+        inputBoxIsVisible(addNewBtn);
+        searchRoomForExtraCost(bookingIdKeys, roomTitleKeys);
         String bookingDetails = driver.findElement(bookingDetailsText).getText();
-        Assertions.assertEquals(bookingDetails, bookingDetails, "The extra costs were not added correctly.");
+        Assertions.assertEquals("Booking Details", bookingDetails);
     }
 
     @Test
-    public void verifyThatThe(){
-
+    public void verifyThatTheCalculateBtnCalculatesTheAmountOfExtraCosts() throws InterruptedException {
+        inputBoxIsVisible(addNewBtn);
+        searchRoomForExtraCost(bookingIdKeys, roomTitleKeys);
+        calculateTheExpenses(particularData, amountData, quantityData);
+        String expenseCalculatedData = driver.findElement(expenseSubTotal).getAttribute("value");
+        Assertions.assertEquals(expenseCalculatedData, expenseSubTotalInput);
     }
 
+    @Test
+    public void verifyThatTheSubmitBtnSubmitsTheExpenses() throws InterruptedException {
+        inputBoxIsVisible(addNewBtn);
+        searchRoomForExtraCost(bookingIdKeys, roomTitleKeys);
+        calculateTheExpenses(particularData, amountData, quantityData);
+        WebElement expenseSubmit = driver.findElement(expenseSubmitBtn);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", expenseSubmit);
+        Thread.sleep(2000);
+        expenseSubmit.click();
+        Thread.sleep(2000);
+        String extraCostWebElement = driver.findElement(extraCostsToast).getText();
+        Assertions.assertEquals(extraCostWebElement, "Extra Cost added successfully");
+    }
 
+    @Test
+    public void verifyThatTheExpenseIsEditable(){
+        driver.findElement(editBtn).click();
+        String editExtraCostPage = driver.findElement(editExtCost).getText();
+        Assertions.assertEquals("Update Payment", editExtraCostPage);
+    }
+
+    @Test
+    public void verifyThatWhenTheValueOfPaidAmountIsKeptTheUpdateButtonIsVisible() throws InterruptedException {
+        driver.findElement(editBtn).click();
+        driver.findElement(paidAmount).sendKeys(discountAmtInput);
+        Thread.sleep(2000);
+        String paidAmt = driver.findElement(updateBtn).getText();
+        Assertions.assertEquals("Update", paidAmt);
+    }
+
+    @Test
+    public void verifyThatTheUpdateButtonUpdatesTheStatusAfterThePaymentIsDone() throws InterruptedException {
+        driver.findElement(editBtn).click();
+        driver.findElement(paidAmount).sendKeys(discountAmtInput);
+        Thread.sleep(2000);
+        driver.findElement(updateBtn).click();
+        String updatedStatus = driver.findElement(extraCostsToast).getText();
+        Assertions.assertEquals("Updated successful!", updatedStatus);
+    }
+
+    @Test
+    public void verifyThatTheDeleteIconProcessesTheDelete() throws InterruptedException {
+        driver.findElement(deleteBtn).click();
+        String confirmDelete = driver.findElement(confirmDeleteText).getText();
+        Assertions.assertEquals("Are you sure?", confirmDelete);
+        driver.findElement(confirmDeleteBtn).click();
+        String deleteConfirmation = driver.findElement(extraCostsToast).getText();
+        Assertions.assertEquals("Extra Cost deleted successfully", deleteConfirmation);
+
+    }
 }
--- src/test/java/pages/ExtraCostsPage.java
+++ src/test/java/pages/ExtraCostsPage.java
@@ -14,21 +14,39 @@ import java.time.Duration;
 public class ExtraCostsPage {
     public static WebDriver driver;
     public static final String hotelExtraCosts = "Extra Costs";
+    public static By extraCostsToast = By.xpath("//*[@id=\"toast-container\"]/div/div[2]");
     public static By addNewBtn = By.xpath("/html/body/div[1]/div[1]/div[2]/div/section/div/div/div/div/div[1]/a");
     public static By addNewPageText = By.xpath("/html/body/div[1]/div[1]/div/div/section/div/div/div/div/section/div/div/div/div[1]/h3");
     public static By bookingIDText = By.xpath("//input[@id='bookingId']");
     public static By roomTitleText = By.xpath("//input[@id='roomTitle']");
     public static By searchBtn = By.xpath("//button[@id='searchButton']");
     public static By pickDate = By.xpath("//*[@id='date']");
-    public static By bookingDetailsText = By.xpath("//*[@id=\"expensesSection\"]/div[1]");
+    public static By bookingDetailsText = By.xpath("//h5[contains(text(),'Booking Details')]");
     public static By bookingId = By.xpath("//input[@id='bookingId']");
     public static By roomTitleInput = By.xpath("//*[@id=\"roomTitle\"]");
-    public static By editExtCosts = By.xpath("//*[@id=\"editExtCosts\"]");
-    public static By deletePopUp = By.xpath("//*[@id=\"deletePopUp\"]");
-    public static By deleteExtCosts = By.xpath("//*[@id=\"deleteExtCosts\"]");
+    public static By error_msg = By.xpath("//*[@id=\"message\"]");
+    public static By expenseParticular = By.xpath("//*[@id=\"item1\"]/div/div[1]/input");
+    public static By expenseAmount = By.xpath("//*[@id=\"item1\"]/div/div[2]/input");
+    public static By expenseQuantity = By.xpath("//*[@id=\"item1\"]/div/div[3]/input");
+    public static By expenseCalculateBtn = By.xpath("//*[@id=\"calculateBtn\"]");
+    public static By expenseSubTotal = By.xpath("//*[@id=\"subtotal\"]");
+    public static By expenseSubmitBtn = By.xpath("//*[@id=\"button_submit\"]");
+    public static By editExtCost = By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div/div/div[2]/h4");
+    public static By paidAmount = By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div/div/div[2]/input[5]");
+    public static By editBtn = By.xpath("//tbody/tr[4]/td[8]/div[1]/a[2]/i[1]");
+    public static By updateBtn = By.xpath("/html/body/div[1]/div[1]/div/div/div/div/div/div/div[2]/div[1]/button");
+    public static By deleteBtn = By.xpath("//tbody/tr[7]/td[8]/div[1]/form[1]/button[1]");
+    public static By confirmDeleteText = By.xpath("//div[@id='swal2-html-container']");
+    public static By confirmDeleteBtn = By.xpath("//button[contains(text(),'Yes, delete it!')]");
     public static final String bookingIdKeys = "7";
     public static final String roomTitleKeys = "405";
     public static final String dateKeys = "2025-01-14";
+    public static final String particularData = "Laundry";
+    public static final String amountData = "150";
+    public static final String quantityData = "5";
+    public static final String expenseSubTotalInput = "750";
+    public static final String discountAmtInput = "750";
+
 
     public ExtraCostsPage(WebDriver newDriver) {
         driver = newDriver;
@@ -41,25 +59,35 @@ public class ExtraCostsPage {
         Assertions.assertEquals(expectedExtraCosts, hotelExtraCosts);
     }
 
-    public static void selectDate() throws InterruptedException {
+    public static void selectDate(String dateInput) throws InterruptedException {
         WebElement date = driver.findElement(pickDate);
-        date.sendKeys(dateKeys);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].value = '2024-06-11';", dateKeys);
+        date.sendKeys(dateInput);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].value = '2024-06-11'", dateKeys);
         Thread.sleep(2000);
     }
 
-    public static void addExtraCost() throws InterruptedException {
-        driver.findElement(addNewBtn).click();
-        selectDate();
-        driver.findElement(bookingIDText).sendKeys(bookingIdKeys);
-        driver.findElement(roomTitleText).sendKeys(roomTitleKeys);
+    public static void searchRoomForExtraCost(String bookingId, String roomTitle) throws InterruptedException {
+        selectDate(dateKeys);
+        driver.findElement(bookingIDText).sendKeys(bookingId);
+        driver.findElement(roomTitleText).sendKeys(roomTitle);
         driver.findElement(searchBtn).click();
         Thread.sleep(2000);
     }
 
-    public static void addBtnIsVisible(By element ){
+    public static void inputBoxIsVisible(By element ){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
         wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+        driver.findElement(element).click();
     }
+
+    public static void calculateTheExpenses(String particular, String amount, String quantity){
+        driver.findElement(expenseParticular).sendKeys(particular);
+        driver.findElement(expenseAmount).sendKeys(amount);
+        WebElement quantityInput = driver.findElement(expenseQuantity);
+        quantityInput.clear();
+        quantityInput.sendKeys(quantity);
+        driver.findElement(expenseCalculateBtn).click();
+    }
+
 
 }
